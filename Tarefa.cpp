9c1011de2dde9d25663616a220becdc766b76c75
Elias Rayes <eliasr@usp.br>
2020-06-29 23:37:00 -0300

    Teste D feito
    Usa número aleatório,
    Seed se baseia no relógio

--- Tarefa.cpp
+++ Tarefa.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 #include <iostream>
 #include <sstream>
+#include <chrono>
+#include <random>
 
 Tarefa::Tarefa(int N, int M) : N(N), M(M)
 {
@@ -28,12 +30,14 @@ void Tarefa::arquivo()
 	entrada.open("teste.txt");
 
 	// Verificar se o arquivo existe
-	if (entrada.fail()) {
+	if (entrada.fail())
+	{
 		entrada.close();
 		throw new std::invalid_argument("Arquivo nao encontrado");
 	}
 
-	if (entrada.eof()) {
+	if (entrada.eof())
+	{
 		entrada.close();
 		throw new std::invalid_argument("Arquivo vazio");
 	}
@@ -44,8 +48,10 @@ void Tarefa::arquivo()
 		p->push_back(std::stod(s));
 
 	uT->resize(N + 1, 0);
-	for (int i = 0; i < 2049; i++) {
-		if (!entrada.good()) {
+	for (int i = 0; i < 2049; i++)
+	{
+		if (!entrada.good())
+		{
 			entrada.close();
 			throw new std::invalid_argument("Erro de formatacao");
 		}
@@ -65,26 +71,44 @@ void Tarefa::MMQ(std::vector<double>* p, char ch)
 	std::vector<std::vector<double>*>* A = new std::vector<std::vector<double>*>(N);
 	std::vector<double>* b = new std::vector<double>(N);
 	std::vector<double>* coef = new std::vector<double>(N);
+
+	std::mt19937 gerador(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+	std::uniform_real_distribution<double> dist(-1.0, 1.0);
+
 	double erroQuadratico = 0, uTCalc = 0;
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		A->at(i) = new std::vector<double>(N);
 		base->at(i) = crankPontual(p->at(i));
 	}
 
-	if (ch == 'a') {
-		for (unsigned int i = 0; i < base->at(0)->size(); i++) {
+	if (ch == 'a')
+	{
+		for (unsigned int i = 0; i < base->at(0)->size(); i++)
+		{
 			uT->push_back(7 * base->at(0)->at(i));
 		}
 	}
-	else if (ch == 'b') {
-		for (unsigned int i = 0; i < base->at(0)->size(); i++) {
+	else if (ch == 'b')
+	{
+		for (unsigned int i = 0; i < base->at(0)->size(); i++)
+		{
 			uT->push_back(2.3 * base->at(0)->at(i) + 3.7 * base->at(1)->at(i) + 0.3 * base->at(2)->at(i) + 4.2 * base->at(3)->at(i));
 		}
 	}
+	else if (ch == 'd')
+	{
+		for (unsigned int i = 0; i < base->at(0)->size(); i++)
+		{
+			uT->at(i) *= (1 + dist(gerador) * 0.01);
+		}
+	}
 
-	for (int i = 0; i < N; i++) {
-		for (int j = i; j < N; j++) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = i; j < N; j++)
+		{
 			double produto = innerProduct(base->at(i), base->at(j));
 			A->at(i)->at(j) = produto;
 			A->at(j)->at(i) = produto;
@@ -95,17 +119,21 @@ void Tarefa::MMQ(std::vector<double>* p, char ch)
 	coef = solveLDLt(A, b);
 	printLine(*coef, std::cout);
 
-	for (unsigned int i = 1; i < uT->size() - 1; i++) {
+	for (unsigned int i = 1; i < uT->size() - 1; i++)
+	{
 		uTCalc = 0;
-		for (int k = 0; k < N; k++) {
+		for (int k = 0; k < N; k++)
+		{
 			uTCalc += coef->at(k) * base->at(k)->at(i);
 		}
 		erroQuadratico += (uT->at(i) - uTCalc) * (uT->at(i) - uTCalc);
 	}
 	erroQuadratico *= deltaX;
-	std::cout << std::scientific << erroQuadratico << std::endl;
+	std::cout << "Erro Quadratico:" << std::endl
+		<< std::scientific << erroQuadratico << std::endl;
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		delete base->at(i);
 	}
 	delete base;
@@ -126,13 +154,16 @@ std::vector<double>* Tarefa::crankPontual(double p)
 	std::vector<double>* temp = new std::vector<double>(N - 1, 0);
 
 	// Calculo por metodo de Crank-Nicolson
-	for (int k = 0; k < M; k++) {
+	for (int k = 0; k < M; k++)
+	{
 		u->at(0) = 0;
-		for (int i = 0; i < N - 1; i++) {
+		for (int i = 0; i < N - 1; i++)
+		{
 			b->at(i) = (1 - lambda) * uAnterior->at(i + 1) + (lambda / 2) * uAnterior->at(i) + (lambda / 2) * uAnterior->at(i + 2) + (deltaT / 2) * (f(k, i + 1, p) + f(k + 1, i + 1, p));
 		}
 		temp = solveLDLtCN(diag, sub, b);
-		for (int i = 1; i < N; i++) {
+		for (int i = 1; i < N; i++)
+		{
 			u->at(i) = temp->at(i - 1);
 		}
 		u->at(N) = 0;
@@ -169,8 +200,10 @@ double Tarefa::f(int k, int i, double p)
 
 void Tarefa::printLine(std::vector<double> line, std::ostream& output)
 {
-	for (unsigned int i = 0; i < line.size(); i++) {
-		output << std::scientific << line.at(i) << " ";
+	output << std::endl << "Coeficientes:" << std::endl;
+	for (unsigned int i = 0; i < line.size(); i++)
+	{
+		output << "a_" << i << " = " << std::scientific << line.at(i) << std::endl;
 	}
 	output << std::endl;
 }
@@ -178,7 +211,8 @@ void Tarefa::printLine(std::vector<double> line, std::ostream& output)
 double Tarefa::innerProduct(std::vector<double>* u, std::vector<double>* v)
 {
 	double produto = 0;
-	for (unsigned int i = 1; i < u->size() - 1; i++) {
+	for (unsigned int i = 1; i < u->size() - 1; i++)
+	{
 		produto += u->at(i) * v->at(i);
 	}
 	return produto;
@@ -191,7 +225,8 @@ std::vector<double>* Tarefa::solveLDLtCN(std::vector<double>* diag, std::vector<
 	std::vector<double>* l = new std::vector<double>(N - 2, 0);
 
 	double somaD = 0;
-	for (int i = 0; i < N - 2; i++) {
+	for (int i = 0; i < N - 2; i++)
+	{
 		d->at(i) = diag->at(i) - somaD;
 		l->at(i) = sub->at(i) / d->at(i);
 		somaD = l->at(i) * l->at(i) * d->at(i);
@@ -203,19 +238,22 @@ std::vector<double>* Tarefa::solveLDLtCN(std::vector<double>* diag, std::vector<
 	std::vector<double>* x = new std::vector<double>(N - 1, 0);
 
 	double somaX = 0;
-	for (int i = 0; i < N - 2; i++) {
+	for (int i = 0; i < N - 2; i++)
+	{
 		x->at(i) = b->at(i) - somaX;
 		somaX = l->at(i) * x->at(i);
 	}
 	x->at(N - 2) = b->at(N - 2) - somaX;
 
-	for (int i = 0; i < N - 1; i++) {
+	for (int i = 0; i < N - 1; i++)
+	{
 		x->at(i) = x->at(i) / d->at(i);
 	}
 
 	somaX = 0;
 	x->at(N - 2) = x->at(N - 2) - somaX;
-	for (int i = N - 3; i >= 0; i--) {
+	for (int i = N - 3; i >= 0; i--)
+	{
 		somaX = l->at(i) * x->at(i + 1);
 		x->at(i) = x->at(i) - somaX;
 	}
@@ -232,20 +270,25 @@ std::vector<double>* Tarefa::solveLDLt(std::vector<std::vector<double>*>* A, std
 	std::vector<double>* d = new std::vector<double>(N, 0);
 	std::vector<std::vector<double>*>* l = new std::vector<std::vector<double>*>(N);
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		l->at(i) = new std::vector<double>(N);
 		l->at(i)->at(i) = 1;
 	}
 
 	double soma = 0;
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j <= i; j++) {
-			for (int k = 0; k <= j - 1; k++) {
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			for (int k = 0; k <= j - 1; k++)
+			{
 				soma += l->at(j)->at(k) * d->at(k) * l->at(j)->at(k);
 			}
 			d->at(j) = A->at(j)->at(j) - soma;
 			soma = 0;
-			for (int k = 0; k <= j - 1; k++) {
+			for (int k = 0; k <= j - 1; k++)
+			{
 				soma += l->at(i)->at(k) * d->at(k) * l->at(j)->at(k);
 			}
 			l->at(i)->at(j) = (A->at(i)->at(j) - soma) / d->at(j);
@@ -257,27 +300,33 @@ std::vector<double>* Tarefa::solveLDLt(std::vector<std::vector<double>*>* A, std
 
 	std::vector<double>* x = new std::vector<double>(N, 0);
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		soma = 0;
-		for (int k = 0; k <= i - 1; k++) {
+		for (int k = 0; k <= i - 1; k++)
+		{
 			soma += l->at(i)->at(k) * x->at(k);
 		}
 		x->at(i) = b->at(i) - soma;
 	}
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		x->at(i) = x->at(i) / d->at(i);
 	}
 
-	for (int i = N - 1; i >= 0; i--) {
+	for (int i = N - 1; i >= 0; i--)
+	{
 		soma = 0;
-		for (int k = i + 1; k < N; k++) {
+		for (int k = i + 1; k < N; k++)
+		{
 			soma += l->at(k)->at(i) * x->at(k);
 		}
 		x->at(i) = x->at(i) - soma;
 	}
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < N; i++)
+	{
 		delete l->at(i);
 	}
 	delete l;
--- main.cpp
+++ main.cpp
@@ -5,50 +5,62 @@
 void menu();
 
 // Menu visual para recebimento de instrucoes
-void menu() {
+void menu()
+{
 
 	char item;
 	std::cout << std::endl;
 	std::cout << "Escolha o teste: ";
 	std::cin >> item;
 	item = (char)tolower(item);
-	if (item == 'a') {
+	if (item == 'a')
+	{
 		Tarefa tarefa(128, 128);
 		std::vector<double>* p = new std::vector<double>{ 0.35 };
 		tarefa.MMQ(p, 'a');
 	}
-	else if (item == 'b') {
+	else if (item == 'b')
+	{
 		Tarefa tarefa(128, 128);
-		std::vector<double>* p = new std::vector<double>{ 0.15 , 0.3, 0.7, 0.8 };
+		std::vector<double>* p = new std::vector<double>{ 0.15, 0.3, 0.7, 0.8 };
 		tarefa.MMQ(p, 'b');
 	}
-	else if (item == 'c') {
+	else if (item == 'c')
+	{
 		int N;
 		std::cout << std::endl << "Digite o valor de N = M: ";
 		std::cin >> N;
-		if (!std::cin) {
+		if (!std::cin)
+		{
 			std::cin.clear();
 			std::cin.ignore(INT_MAX, '\n');
 			throw new std::invalid_argument("Digite um numero");
 		}
 		if (2048 % N != 0)
-			throw new std::invalid_argument("Valor de N invalido!");
+			throw new std::invalid_argument("Valor de N invalido");
 		Tarefa tarefa(N, N);
 		tarefa.arquivo();
 		tarefa.MMQ(tarefa.getP(), 'c');
 	}
-	else if (item == 'd') {
+	else if (item == 'd')
+	{
 		int N;
 		std::cout << std::endl << "Digite o valor de N = M: ";
 		std::cin >> N;
-		if (!std::cin) {
+		if (!std::cin)
+		{
 			std::cin.clear();
 			std::cin.ignore(INT_MAX, '\n');
 			throw new std::invalid_argument("Digite um numero");
 		}
+		if (2048 % N != 0)
+			throw new std::invalid_argument("Valor de N invalido");
 		Tarefa tarefa(N, N);
+		tarefa.arquivo();
+		tarefa.MMQ(tarefa.getP(), 'd');
 	}
-	else {
+	else
+	{
 		throw new std::invalid_argument("Digite apenas uma letra de A a D");
 	}
 	menu();
@@ -61,12 +73,15 @@ int main()
 	std::cout << "Exercicio Programa 2" << std::endl;
 	std::cout << "====================" << std::endl;
 	bool exception = 0;
-	do {
-		try {
+	do
+	{
+		try
+		{
 			exception = 0;
 			menu();
 		}
-		catch (std::exception* e) {
+		catch (std::exception* e)
+		{
 			std::cout << std::endl << "** Erro na execucao do programa: " << e->what() << "! **" << std::endl;
 			delete e;
 			exception = 1;
