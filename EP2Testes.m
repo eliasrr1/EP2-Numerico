ed489c1f2d512078f4db70e86dbab2157fa8b212
Elias Rayes <eliasr@usp.br>
2020-07-02 22:43:00 -0300

    Adicionar pontos que indicam as fontes

--- EP2Testes.m
+++ EP2Testes.m
@@ -3,26 +3,36 @@
 N = 128;
 x = linspace(0, 1, N + 1);
 U = readmatrix('OutputA128.txt');
+p = 0.35;
 
 figure;
 hold
 title("Teste A");
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,'FigA128.png')
 
 %% Teste B
 
 N = 128;
 x = linspace(0, 1, N + 1);
 U = readmatrix('OutputB128.txt');
+p = [0.15; 0.30; 0.70; 0.80];
 
 figure;
 hold
 title("Teste B");
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,'FigB128.png')
+
+%% Para os testes C e D
+p = [0.14999999999999999; 0.20000000000000001; 0.29999999999999999; 0.34999999999999998; 0.50000000000000000
+     0.59999999999999998; 0.69999999999999996; 0.72999999999999998; 0.84999999999999998; 0.90000000000000002];
 
 %% Teste C
 N = 128;
@@ -35,7 +45,9 @@ hold
 title("Teste C com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigC" + N + ".png")
 
 %% Teste C
 N = 256;
@@ -48,7 +60,9 @@ hold
 title("Teste C com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigC" + N + ".png")
 
 %% Teste C
 N = 512;
@@ -61,7 +75,9 @@ hold
 title("Teste C com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigC" + N + ".png")
 
 %% Teste C
 N = 1024;
@@ -74,7 +90,9 @@ hold
 title("Teste C com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigC" + N + ".png")
 
 %% Teste C
 N = 2048;
@@ -87,7 +105,9 @@ hold
 title("Teste C com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigC" + N + ".png")
 
 %% Teste D
 N = 128;
@@ -100,7 +120,9 @@ hold
 title("Teste D com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigD" + N + ".pdf")
 
 %% Teste D
 N = 256;
@@ -113,7 +135,9 @@ hold
 title("Teste D com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigD" + N + ".pdf")
 
 %% Teste D
 N = 512;
@@ -126,7 +150,9 @@ hold
 title("Teste D com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigD" + N + ".pdf")
 
 %% Teste D
 N = 1024;
@@ -139,7 +165,9 @@ hold
 title("Teste D com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigD" + N + ".pdf")
 
 %% Teste D
 N = 2048;
@@ -152,4 +180,6 @@ hold
 title("Teste D com N = " + N);
 plot(x, U(:,1));
 plot(x, U(:,2));
+plot(p, U(ceil(N*p),1),'bo');
 legend('uT Medido','uT Reconstruído')
+saveas(gcf,"FigD" + N + ".pdf")
